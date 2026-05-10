13c47beb5cbb492cef8ec9c26e9ab49070de3885
eremognosis <106125091@nitt.edu>
2026-05-10 17:06:33 +0530

    MINOR REFACTOR

--- build-tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raj/Codespace/newIdeas/numER
-# Build directory: /home/raj/Codespace/newIdeas/numER/build-tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build-tests/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raj/Codespace/newIdeas/numER/tests
-# Build directory: /home/raj/Codespace/newIdeas/numER/build-tests/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BasicTest "/home/raj/Codespace/newIdeas/numER/build-tests/tests/test_basic")
-set_tests_properties(BasicTest PROPERTIES  _BACKTRACE_TRIPLES "/home/raj/Codespace/newIdeas/numER/tests/CMakeLists.txt;3;add_test;/home/raj/Codespace/newIdeas/numER/tests/CMakeLists.txt;0;")
